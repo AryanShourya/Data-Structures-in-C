d97b7401fc085f598710d2ed1a274ee4518c2161
Aryan Shourya <aryanshourya98@gmail.com>
2024-10-10 23:06:53 +0530

    deleted file

--- Stack/test.c
+++ Stack/test.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include"stack_int.h"
 
+int main()
+{
+    printf("Demo of header file stack_int.h.\n");
+    stack_int *s1=make_stack(5);
+    int i=1;
+    while(!isFull(s1))
+    {
+        push(s1,i++);
+        printf("Element entered:%d\n",top(s1));
+    }
+
+    push(s1,6);
+    pop(s1);
+    printf("After removing top element, value at the top is:%d",top(s1));
+    free_stack(s1);
+    return 0;
+}
