b5b0867cb43fdb7e04f4b387a046ee279a7bb780
Aryan Shourya <aryanshourya98@gmail.com>
2024-10-10 23:04:17 +0530

    Updated comments

--- Stack/stack_int.h
+++ Stack/stack_int.h
@@ -1,16 +1,15 @@
 #ifndef STACK_INT_H
 #define STACK_INT_H
-// This header file includes function declarations for stack
+// This header file includes function declarations for stack_int 
+// This header file is only to be used for the creation of integer type stacks.
 
-struct stack_int{
-    int *arr;
-    int capacity,size,topindx;
-}typedef stack;
+typedef struct stack_int stack_int;
 
-int isFull(stack* );
-int isEmpty(stack* );
-int top(stack* );
-void pop(stack* );
-void push(stack *);
-stack* make_stack(char ,int );
+int isFull(stack_int* );
+int isEmpty(stack_int* );
+int top(stack_int* );
+void pop(stack_int* );
+void push(stack_int *,int);
+stack_int* make_stack(int );
+void free_stack(stack_int* );
 #endif 
