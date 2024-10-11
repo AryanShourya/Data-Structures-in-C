4493faf0914084149192aa9e9c9e2c814cb1c1d1
Aryan Shourya <aryanshourya98@gmail.com>
2024-10-12 02:37:09 +0530

    added stack method
    added functions to create all data type stacks

--- Stack/stack_int.h
+++ Stack/stack_int.h
@@ -1,15 +1,22 @@
 #ifndef STACK_INT_H
 #define STACK_INT_H
-// This header file includes function declarations for stack_int 
-// This header file is only to be used for the creation of integer type stacks.
+// This header file includes file to implement stack in c program files.
 
+
+typedef enum dataflag dataflag;
 typedef struct stack_int stack_int;
 
 int isFull(stack_int* );
 int isEmpty(stack_int* );
-int top(stack_int* );
+int top_int(stack_int* );
+float top_float(stack_int* );
+double top_double(stack_int* );
+char top_char(stack_int* );
 void pop(stack_int* );
-void push(stack_int *,int);
-stack_int* make_stack(int );
+void push_int(stack_int *,int);
+void push_float(stack_int *,float);
+void push_double(stack_int *,double);
+void push_char(stack_int *,char);
+stack_int* create_stack(char ,int );
 void free_stack(stack_int* );
 #endif 
