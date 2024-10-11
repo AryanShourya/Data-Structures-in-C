06f0d565a8b7c65e0942cbeab36916b812307c13
Aryan Shourya <aryanshourya98@gmail.com>
2024-10-12 02:38:33 +0530

    added stack func
    added individual func for int,float,double,char data types

--- Stack/stack_int.c
+++ Stack/stack_int.c
@@ -3,15 +3,21 @@
 #include<stdlib.h>
 #include<conio.h>
 #include"stack_int.h"
-
+enum dataflag{
+    i,
+    f,
+    d,
+    c 
+};
 struct stack_int{
-    int* arr;
+    void* arr;
+    dataflag df;
     int size;
     int capacity;
     int topindx;
 };
 
-stack_int* make_stack(int s)
+stack_int* create_stack(char c,int s)
 {
     // use dynamic memory allocation to allocate memory for stack struct.
     stack_int *s1= (stack_int*)malloc(sizeof(stack_int)*1);
@@ -21,7 +27,29 @@ stack_int* make_stack(int s)
     s1->size=0;
     // set top index to -1 to determine underflow and overflow.
     s1->topindx=-1;
-    s1->arr=(int*)malloc(sizeof(int)*s);
+    switch (c){
+        case 'i':
+        s1->arr=(int*)malloc(sizeof(int)*s);
+        s1->df=i;
+        break;
+        case 'f':
+        s1->arr=(float*)malloc(sizeof(float)*s);
+        s1->df=f;
+        break;
+        case 'd':
+        s1->arr=(double*)malloc(sizeof(double)*s);
+        s1->df=d;
+        break;
+        case 'c':
+        s1->arr=(char*)malloc(sizeof(char)*s);
+        s1->df=c;
+        break;
+        default:
+        printf("Enter valid data type!\n");
+        free_stack(s1);
+        return NULL;
+    }
+    
     return s1; 
 }
 
@@ -37,21 +65,90 @@ int isEmpty(stack_int* s1)
     return s1->size==0;
 }
 
-//returns the top element of the stack
-int top(stack_int* s1)
+//returns the top integer element of the stack
+int top_int(stack_int* s1)
 {
     if(isEmpty(s1))
     {
         printf("The stack is empty!\n");
         return 1;
     }
+
+    if(s1->df!=0)
+    {
+        printf("Cannot access non-integer elements!\nuse top_int()");
+        exit(0);
+    }
+    if(s1->df!=0)
+    {
+        printf("Cannot access top of stack! Wrong function used.\n");
+        return 1;
+    }
     
-    return s1->arr[s1->topindx];
+    return ((int*)s1->arr)[s1->topindx];
 }
 
-//function to insert into the stack
-void push(stack_int *s1,int n)
+// returns top float of the stack
+float top_float(stack_int* s1)
 {
+    if(isEmpty(s1))
+    {
+        printf("The stack is empty!\n");
+        return 1;
+    }
+
+    if(s1->df!=1)
+    {
+        printf("Cannot access top of stack! Wrong function used.\n");
+        return 0.0;
+    }
+    
+    return ((float*)s1->arr)[s1->topindx];
+}
+
+double top_double(stack_int* s1)
+{
+    if(isEmpty(s1))
+    {
+        printf("The stack is empty!\n");
+        return 1;
+    }
+
+    if(s1->df!=2)
+    {
+        printf("Cannot access top of stack! Wrong function used.\n");
+        return 0.0000;
+    }
+    
+    return ((double*)s1->arr)[s1->topindx];
+}
+
+char top_char(stack_int* s1)
+{
+    if(isEmpty(s1))
+    {
+        printf("The stack is empty!\n");
+        return '\0';
+    }
+
+    if(s1->df!=3)
+    {
+        printf("Cannot access top of stack! Wrong function used.\n");
+        return '\0';
+    }
+    
+    return ((char*)s1->arr)[s1->topindx];
+}
+
+//function to insert integer into the stack
+void push_int(stack_int *s1,int n)
+{
+    if(s1->df!=0)
+    {
+        printf("cannot insert different element!\n");
+        return;
+    }
+
     if(isFull(s1))
     {
         printf("Stack is full! OVERFLOW!\n");
@@ -59,7 +156,63 @@ void push(stack_int *s1,int n)
     }
     s1->topindx++;
     s1->size++;
-    s1->arr[s1->topindx]=n;
+    ((int*)s1->arr)[s1->topindx]=n;
+}
+
+// function to insert flaot in stack
+void push_float(stack_int *s1,float n)
+{
+    if(s1->df!=1)
+    {
+        printf("cannot insert different element!\n");
+        return;
+    }
+    if(isFull(s1))
+    {
+        printf("Stack is full! OVERFLOW!\n");
+        return;
+    }
+    s1->topindx++;
+    s1->size++;
+    ((float*)s1->arr)[s1->topindx]=n;
+}
+
+// push double element into the stack
+void push_double(stack_int *s1,double n)
+{
+    if(s1->df!=2)
+    {
+        printf("cannot insert different element!\n");
+        return;
+    }
+
+    if(isFull(s1))
+    {
+        printf("Stack is full! OVERFLOW!\n");
+        return;
+    }
+    s1->topindx++;
+    s1->size++;
+    ((double*)s1->arr)[s1->topindx]=n;
+}
+
+// push character element into the stack
+void push_char(stack_int *s1,char n)
+{
+    if(s1->df!=3)
+    {
+        printf("cannot insert different element!\n");
+        return;
+    }
+
+    if(isFull(s1))
+    {
+        printf("Stack is full! OVERFLOW!\n");
+        return;
+    }
+    s1->topindx++;
+    s1->size++;
+    ((char*)s1->arr)[s1->topindx]=n;
 }
 
 //function to delete element from stack
