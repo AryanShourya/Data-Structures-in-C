ef32e48091d61abedb8f9aedf1dc3f6f7de976b7
Aryan Shourya <aryanshourya98@gmail.com>
2024-10-14 02:48:17 +0530

    added func

--- queue/queue.h
+++ queue/queue.h
@@ -2,13 +2,21 @@
 #define QUEUE_H_
 
 enum dataflag;
-struct node;
-struct queue;
+typedef struct node node;
+typedef struct queue queue;
 
 queue* create_queue(char );
 void enqueue_int(queue* ,int );
+void enqueue_float(queue* ,float );
+void enqueue_double(queue* ,double );
+void enqueue_char(queue* ,char );
 void dequeue(queue* );
 int front_int(queue* );
-void free_queue(queue* );
+float front_float(queue* );
+double front_double(queue* );
+char front_char(queue* );
+int isQueueEmpty(queue* );
+//void free_queue(queue* );
+void disp_queue(queue* q1);
 
 #endif 
