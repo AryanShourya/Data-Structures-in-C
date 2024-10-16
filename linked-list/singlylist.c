f83eca78f057e5011216de052543ad9e1bf35319
Aryan Shourya <aryanshourya98@gmail.com>
2024-10-17 04:34:48 +0530

    added func

--- linked-list/singlylist.c
+++ linked-list/singlylist.c
@@ -19,7 +19,7 @@ struct slist{
     node* head;
 };
 
-void create_slist(char x)
+slist* create_slist(char x)
 {
     // allocate memory for linked list
     slist* s1=(slist*)malloc(sizeof(slist));
@@ -60,16 +60,27 @@ void insert_slist_int(slist* s1,int val,int pos)
 
     if(s1->head==NULL)
     {
-        s1->head= malloc(sizeof(node));
+        s1->head= (node*)malloc(sizeof(node));
+        s1->head->data= (int*)malloc(sizeof(int));
         *(int*)(s1->head->data)= val;
         s1->head->next=NULL;
         return;
     }
 
+    if(pos==0)
+    {
+        node* newNode=malloc(sizeof(node));
+        newNode->data=(int*)malloc(sizeof(int));
+        *(int*)(newNode->data)= val;
+        newNode->next=s1->head;
+        s1->head=newNode;
+        printf("head");
+        return;
+    }
     node* ptr=NULL;
     ptr=s1->head;
 
-    while(pos!=0)
+    while(pos!=0 && ptr!=NULL)
     {
         ptr=ptr->next;
         pos--;
@@ -82,6 +93,7 @@ void insert_slist_int(slist* s1,int val,int pos)
     }
 
     node* newNode=malloc(sizeof(node));
+    newNode->data=(int*)malloc(sizeof(int));
     *(int*)(newNode->data)= val;
     newNode->next = ptr->next;
     ptr->next=newNode;
@@ -103,18 +115,18 @@ void disp_slist(slist* s1 )
    { 
     switch (s1->df){
         case 0:
-        printf("%d ",*(int*)(s1->data));
+        printf("%d ",*(int*)(ptr->data));
         break;
         case 1:
-        printf("%f ",*(float*)(s1->data));
+        printf("%f ",*(float*)(ptr->data));
         break;
 
         case 2:
-        printf("%lf ",*(double*)(s1->data));
+        printf("%lf ",*(double*)(ptr->data));
         break;
 
         case 3:
-        printf("%c ",*(char*)(s1->data));
+        printf("%c ",*(char*)(ptr->data));
 
         default:
         return;
@@ -125,4 +137,40 @@ void disp_slist(slist* s1 )
     printf("NULL\n");
 }
 
-// 
+// delete node
+void remove_slist(slist* s1,int pos)
+{
+    if(s1->head==NULL)
+    {
+        printf("Linked list is empty!\n");
+        return;
+    }
+
+    if(pos == 0)
+    {
+        node* temp=s1->head;
+        s1->head=s1->head->next;
+        free(temp);
+        return;
+    }
+
+
+    node* ptr=s1->head;
+    node* prev=NULL;
+    while(pos!=0 && ptr!=NULL)
+    {
+        prev=ptr;
+        ptr=ptr->next;
+        pos--;
+    }
+
+    if(ptr==NULL)
+    {
+        printf("Out of bound!\n");
+        return;
+    }
+
+    prev->next=ptr->next;
+    free(ptr);
+
+}
