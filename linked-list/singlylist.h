d76628cc90bd9cef44dcb254ab50adee65af4674
Aryan Shourya <aryanshourya98@gmail.com>
2024-10-18 04:37:08 +0530

    updated func
    created functions for all data types. (insert function)

--- linked-list/singlylist.h
+++ linked-list/singlylist.h
@@ -10,5 +10,8 @@ void disp_slist(slist* );
 void rdisp_slist(slist* );
 slist* create_slist(char );
 void insert_slist_int(slist* ,int ,int );   // slist ,value , position
+void insert_slist_float(slist* ,float ,int ); 
+void insert_slist_double(slist* ,double ,int ); 
+void insert_slist_char(slist* ,char ,int ); 
 void remove_slist(slist* ,int );            // slist, position
 #endif
